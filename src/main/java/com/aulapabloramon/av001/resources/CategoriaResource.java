914e0c3bea74cd887b7820193a0708baca24dfc5
Aluno <Aluno@acad.unipbfpb.com.br>
2018-03-22 10:53:39 -0300

    Primeira Parte _ Pablo Subiu

---
+++ src/main/java/com/aulapabloramon/av001/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.aulapabloramon.av001.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="avaliacao")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Avaliação Terminada, professor Chimpa!";
+		}
+}
